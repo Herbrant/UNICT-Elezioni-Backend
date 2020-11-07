d030a11216aba922dfe705f025244c3d104b5b48
herbrant <cdavide98carnemolla@gmail.com>
2020-11-07 16:55:17 +0100

    chore(actions): remove unused home action

--- actions/app.go
+++ actions/app.go
@@ -61,7 +61,6 @@ func App() *buffalo.App {
 		// Remove to disable this.
 		app.Use(popmw.Transaction(models.DB))
 
-		app.GET("/", HomeHandler)
 		app.Resource("/organoes", OrganoesResource{})
 	}
 
