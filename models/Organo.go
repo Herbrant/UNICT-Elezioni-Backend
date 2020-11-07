a7e02b4e6d179affddbd3f7621dd5c816086e22c
herbrant <cdavide98carnemolla@gmail.com>
2020-11-07 16:21:14 +0100

    feat(models): add organo models

---
+++ models/Organo.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop/v5"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+)
+
+// Organo is used by pop to map your organoes database table to your go code.
+type Organo struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (o Organo) String() string {
+	jo, _ := json.Marshal(o)
+	return string(jo)
+}
+
+// Organoes is not required by pop and may be deleted
+type Organoes []Organo
+
+// String is not required by pop and may be deleted
+func (o Organoes) String() string {
+	jo, _ := json.Marshal(o)
+	return string(jo)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (o *Organo) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (o *Organo) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (o *Organo) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
